63d2f32a4fa563f9acb9101c39880cbfa3b07d56
Shafly Hamzah <shafly2000@gmail.com>
2022-03-21 18:26:53 +0700

    Add slider-v1 component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+// import { ref, defineProps } from 'vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import SliderV1 from './components/Slider/SliderV1.vue'
+
+const imagesSlider = ['/src/assets/slider1_0.png', '/src/assets/slider1_1.png', '/src/assets/slider1_2.png']
+
 </script>
 
 <template>
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
+  <SliderV1 :slides="imagesSlider"></SliderV1>
 </template>
---
+++ src/components/Slider/SliderItemV1.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { defineProps, computed } from 'vue'
+
+const props = defineProps({
+    slide: String, currentSlide: Number, index: Number, direction: String
+})
+
+const transitionEffect = computed(() => {
+    return props.direction === 'right' ? 'slide-out' : 'slide-in'
+})
+
+</script>
+
+
+<template>
+    <transition :name="transitionEffect">
+        <div class="absolute" v-show="currentSlide === index">
+            <img :src="slide" :alt="`slide-${index}`" />
+        </div>
+    </transition>
+</template>
+
+<style scoped>
+.slide-in-enter-active,
+.slide-in-leave-active,
+.slide-out-enter-active,
+.slide-out-leave-active {
+    transition: all 1s ease-in-out;
+}
+.slide-in-enter-from {
+    transform: translateX(-100%);
+}
+.slide-in-leave-to {
+    transform: translateX(100%);
+}
+.slide-out-enter-from {
+    transform: translateX(100%);
+}
+.slide-out-leave-to {
+    transform: translateX(-100%);
+}
+</style>
