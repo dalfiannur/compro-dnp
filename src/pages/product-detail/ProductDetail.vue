0c59785c138a320c731467bc074b94b90b1e1b2f
Shafly Hamzah <shafly2000@gmail.com>
2022-04-04 14:26:46 +0700

    import sliderv2

--- src/pages/product-detail/ProductDetail.vue
+++ src/pages/product-detail/ProductDetail.vue
@@ -1,5 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+// Import Components
+import SliderV2 from '../../components/Slider/SliderV2/SliderV2.vue'
+import useGetSliderV2 from '../../composable/useGetSliderV2'
+
+const { data: sliderv2 } = useGetSliderV2()
+</script>
+
 <template>
     <div>
         <h1>Product Detail</h1>
+    </div>
+    <div id="banner">
+        <SliderV2 :items="sliderv2" />
     </div>
 </template>
