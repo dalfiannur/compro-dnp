a9373c25c3f677552fb7b0762134ea0c007eb7ae
yudaono <yudaono@gmail.com>
2022-04-07 13:27:12 +0700

    product Banner

--- src/pages/product-detail/components/ProductBanner.vue
+++ src/pages/product-detail/components/ProductBanner.vue
@@ -1,23 +1,23 @@
 <template>
-  <div class="relative py-5 bg-hydrate flex flex-row items-center h-96 flex-grow">
+  <div class="relative py-5 bg-hydrate flex flex-row items-center h-80 lg:h-96 flex-grow">
 
-    <div class="absolute w-72 -rotate-90 border border-white -left-[7.7rem] ">
-      <div class="my-3 uppercase font-questrial tracking-[1rem] text-white">
-        <p class="text-center">
-          glow
+    <div class="absolute w-60 lg:w-72 -rotate-90 border-[0.13rem] border-white -left-[6.2rem] lg:-left-[7.7rem]">
+      <div class="my-2.5 uppercase font-questrial tracking-[0.5rem] text-white">
+        <p class="text-center text-base">
+          hydrate
         </p>
       </div>
     </div>
     
-    <div class="items-center ml-20 mr-10 flex flex-row">
-      <div>
+    <div class="items-center flex">
+      <div class="ml-20 mr-10">
         logo
       </div>
-      <h2 class="text-7xl font-questrial font-normal text-white">
+      <h2 class="text-7xl lg:text-8xl font-questrial font-normal text-white w-96 lg:w-[">
         Caffeine Hydrating
       </h2>
     </div>
-    <div class="mx-6">
+    <div class="grow">
       <p class="text-xl font-questrial font-normal text-white">
         Caffeine Hydrating
       </p>
