af16c7059abfa425cb5bff44af5b124c2a5e0ae7
Mohamad Fauzi <yudaono@gmail.com>
2022-04-07 23:29:43 +0700

    ProductBanner Initiation

--- src/pages/product-detail/components/ProductBanner.vue
+++ src/pages/product-detail/components/ProductBanner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative py-5 bg-hydrate flex flex-row items-center h-80 lg:h-96 flex-grow">
+  <div class="relative py-5 bg-hydrate flex items-center justify-center h-80 lg:h-96 w-full flex-grow">
 
     <div class="absolute w-60 lg:w-72 -rotate-90 border-[0.13rem] border-white -left-[6.2rem] lg:-left-[7.7rem]">
       <div class="my-2.5 uppercase font-questrial tracking-[0.5rem] text-white">
@@ -9,17 +9,13 @@
       </div>
     </div>
     
-    <div class="items-center flex">
-      <div class="ml-20 mr-10">
-        logo
-      </div>
-      <h2 class="text-7xl lg:text-8xl font-questrial font-normal text-white w-96 lg:w-[">
-        Caffeine Hydrating
+    <div class="items-center lg:flex text-center lg:text-left">
+      <h2 class="text-6xl lg:text-7xl font-questrial font-normal text-white w-96">
+        Tranex & Kojic with B3
       </h2>
-    </div>
-    <div class="grow">
-      <p class="text-xl font-questrial font-normal text-white">
-        Caffeine Hydrating
+      
+      <p class="text-xl lg:text-3xl font-questrial font-normal w-96 text-white mt-8">
+        Dark spot reducer with triple action
       </p>
     </div>
   </div>
