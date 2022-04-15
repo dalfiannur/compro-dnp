87d424438f800d8105a109d1a6fe8c3ca219628b
Dikry Alfiannur Wahyu <dalfiannur@gmail.com>
2022-04-16 02:16:13 +0700

    Update

--- src/pages/home/components/ProductList/Button.vue
+++ src/pages/home/components/ProductList/Button.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref, toRefs } from 'vue';
+import { computed, inject, ref, toRefs, watch } from "vue";
 
 interface Prop {
   text: string;
@@ -7,42 +7,29 @@ interface Prop {
   selected: string;
 }
 
-const props = defineProps<Prop>()
-const hovered = ref<boolean>(false)
+const props = defineProps<Prop>();
+const hovered = ref<boolean>(false);
 
-const { text, category } = toRefs(props)
+const { text, category } = toRefs(props);
 
-const hoverStyle = {
-  undefined: 'text-gray-500 border-gray-500',
-  repair: 'border-repair text-repair',
-  prevent: 'border-prevent text-prevent',
-  glow: 'border-glow text-glow',
-  hydrate: 'border-hydrate text-hydrate',
-}
+const isHovered = inject("isHovered", ref<boolean>(false));
+const hoverCategory = inject("hoverCategory", ref<string>("series"));
+const selectedCategory = inject("selectedCategory", ref<string>("series"));
 
-const activeStyle = {
-  undefined: '',
-  repair: 'bg-repair text-white',
-  prevent: 'bg-prevent text-white',
-  glow: 'bg-glow text-white',
-  hydrate: 'bg-hydrate text-white',
-}
-
-defineExpose({
-  onHover() {
-    hovered.value = true
-  },
-  onBlur() {
-    hovered.value = false
-  }
-})
+watch([selectedCategory, category], ([a, b]) => {
+  console.log(a, b);
+});
 </script>
 <template>
-  <button class="flex-1 py-3 font-bold tracking-[5px]  uppercase md:border-2  h-14"
+  <button
+    class="flex-1 py-3 font-bold tracking-[5px] uppercase md:border-2 h-14 text-gray-500 border-gray-500"
     :class="{
-      [hoverStyle[category]]: selected !== category && hovered ,
-      [hoverStyle['undefined']]: selected !== category && !hovered,
-      [activeStyle[category]] : selected === category
+      [`!border-${hoverCategory} !text-${hoverCategory}`]:
+        selectedCategory !== category &&
+        (isHovered || hovered) &&
+        hoverCategory === category,
+      [`!border-${selectedCategory} bg-${selectedCategory} !text-white`]:
+        selectedCategory === category,
     }"
     @mouseenter="hovered = true"
     @mouseleave="hovered = false"
