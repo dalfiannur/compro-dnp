4f08b199acb477ca2d4472af8e804ce0380e5dc3
Dikry Alfiannur Wahyu <dalfiannur@gmail.com>
2022-04-16 04:51:45 +0700

    Update Product Slider

--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -1,12 +1,14 @@
 <script lang="ts" setup>
-import Header from '../components/Header/Header.vue'
-import Footer from '../components/Footer.vue'
+import Header from "../components/Header/Header.vue";
+import Footer from "../components/Footer.vue";
 </script>
 
 <template>
+  <div>
     <Header />
-    <div class="mt-24">
-        <slot />
+    <div class="my-24">
+      <slot />
     </div>
-    <Footer class="mt-24" />
+    <Footer />
+  </div>
 </template>
