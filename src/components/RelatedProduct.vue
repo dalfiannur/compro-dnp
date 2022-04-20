7229a5d38d7d81052caad925f37f3a9f35d96796
yudaono <yudaono@gmail.com>
2022-04-20 20:03:09 +0700

    related Product

--- src/components/RelatedProduct.vue
+++ src/components/RelatedProduct.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const item = ref<any[]>([
+const items = ref<any[]>([
     {
         id: 1,
         name: 'Caffein Repair 1',
@@ -38,21 +38,36 @@ const item = ref<any[]>([
             }
         ]
     },
+    {
+        id: 4,
+        name: 'Caffein Hydrate 1',
+        category: {
+            slug: 'hydrate'
+        },
+        images: [
+            {
+                imageSourceUrl: '/img/prevent-antioxidant-resveratrol-a.png'
+            }
+        ]
+    },
 ])
 
 </script>
 
 <template>
     <div class="w-full justify-center">
-        <div class="text-center">
-            <h1>Related Product</h1>
+        <div class="text-center my-14">
+            <h1 class="font-questrial text-2xl text-gray-1">Related Product</h1>
         </div>
-        <div class="flex-none">
-            <div v-for="(product, index) in item" :key="index">
-                <div>
-                    <div>
-                        <img :src="product[index].images.imageSourceUrl" alt="">
+        <div class="flex flex-wrap lg:mx-20 justify-center gap-6">
+            <div class="w-72" v-for="(item, index) in items" :key="index">
+                <div class="">
+                    <div class="aspect-square w-full bg-white-smoke">
+                        <img class="p-8" :src="item.images[0].imageSourceUrl" alt="">
                     </div>
+                </div>
+                <div class="my-8">
+                    <p class="font-questrial text-center text-gray-1">{{item.name}}</p>
                 </div>
             </div>
         </div>
