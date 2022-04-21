994eb5c97dd57839dbebe2439fdca2f4a6672859
Shafly Hamzah <shafly2000@gmail.com>
2022-04-21 15:10:37 +0700

    change find clinics button to 'Telusuri'

--- src/pages/clinics/Clinics.vue
+++ src/pages/clinics/Clinics.vue
@@ -12,8 +12,8 @@ import { Clinic } from "../../typings/Clinic";
 const { data: clinics } = useGetQueries<Clinic>("clinics");
 
 type Location = {
-    longitude?: string
-    latitude?: string
+  longitude?: string
+  latitude?: string
 }
 const location = ref<Location>({});
 
@@ -25,11 +25,11 @@ const searchedClinics = computed(() => {
 });
 
 const showMap = (clinic: Clinic) => {
-    const loc = {
-        longitude: clinic.longitude.toString(),
-        latitude: clinic.latitude.toString()
-    }
-    location.value = loc
+  const loc = {
+    longitude: clinic.longitude.toString(),
+    latitude: clinic.latitude.toString()
+  }
+  location.value = loc
 }
 </script>
 
@@ -41,33 +41,22 @@ const showMap = (clinic: Clinic) => {
     <div class="flex-1 max-w-[800px]">
       <!-- Search -->
       <div class="flex justify-center">
-        <div
-          class="relative flex items-stretch w-full mb-5 ml-6 input-group sm:ml-16 mt-7"
-        >
+        <div class="relative flex items-stretch w-full mb-5 ml-6 input-group sm:ml-16 mt-7">
           <img class="w-5" src="/img/search.svg" />
-          <input
-            type="search"
-            v-model="searchQuery"
+          <input type="search" v-model="searchQuery"
             class="form-control font-questrial block w-full px-3 py-1.5 bg-white bg-clip-padding transition ease-in-out m-0 focus:text-gray-500 focus:bg-white focus:border-gray-500 focus:outline-none"
-            placeholder="Cari Klinik / Dermatologist"
-          />
+            placeholder="Cari Klinik / Dermatologist" />
           <button
             class="btn inline-block font-questrial text-sm sm:text-base text-gray-600 mr-5 sm:mr-10 pt-0.5 w-28 sm:w-40 h-11 border border-gray-500"
-            type="button"
-          >
-            Terdekat
+            type="button">
+            Telusuri
           </button>
         </div>
       </div>
       <div class="h-[1px] bg-hydrate"></div>
       <div class="h-[607px] overflow-y-auto">
-        <ClinicItem
-          v-for="(item, index) in clinics"
-          :key="`clinic-${index}`"
-          :data="item"
-          :index="index"
-          @show="showMap(item)"
-        />
+        <ClinicItem v-for="(item, index) in clinics" :key="`clinic-${index}`" :data="item" :index="index"
+          @show="showMap(item)" />
       </div>
     </div>
     <Maps :location="location" />
