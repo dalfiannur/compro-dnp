31d072f81aba1fd07d59165ca48160f5230fdc36
dalfiannur <dalfiannur@gmail.com>
2022-05-18 13:25:53 +0700

    Add Loader Component

--- src/pages/product-detail/ProductDetail.vue
+++ src/pages/product-detail/ProductDetail.vue
@@ -1,39 +1,42 @@
 <script setup lang="ts">
-  import {onMounted, ref} from 'vue'
-  // Import Components
-  // @ts-ignore
-  import SliderV2 from '../../components/Slider/SliderV2/SliderV2.vue'
-  // @ts-ignore
-  import HowToFind from '../../components/HowToFind.vue';
-  // @ts-ignore
-  import ProductBanner from './components/ProductBanner.vue';
-  // @ts-ignore
-  import ProductDescriptions from './components/ProductDescriptions.vue';
-  import useGetSliderV2 from '../../composable/useGetSliderV2'
-  import useGetQuery from "../../composable/useGetQuery";
-  import {Product} from "../../typings/Product";
-  import { useRoute } from 'vue-router';
+import { onMounted, ref } from 'vue'
+// Import Components
+// @ts-ignore
+import SliderV2 from '../../components/Slider/SliderV2/SliderV2.vue'
+// @ts-ignore
+import HowToFind from '../../components/HowToFind.vue';
+// @ts-ignore
+import ProductBanner from './components/ProductBanner.vue';
+// @ts-ignore
+import ProductDescriptions from './components/ProductDescriptions.vue';
+import useGetSliderV2 from '../../composable/useGetSliderV2'
+import useGetQuery from "../../composable/useGetQuery";
+import { Product } from "../../typings/Product";
+import { useRoute } from 'vue-router';
+import Loader from '../../components/Loader.vue';
 
-  const route = useRoute();
-  const {fetcher, data: product} = useGetQuery<Product>('product');
-  const {data: sliderv2} = useGetSliderV2();
+const route = useRoute();
+const { fetcher, data: product, isLoading } = useGetQuery<Product>('product/slug');
+const { data: sliderv2 } = useGetSliderV2();
 
-  onMounted(() => {
-    fetcher(route.params.slug as string)
-  })
+onMounted(() => {
+  fetcher(route.params.slug as string)
+})
 </script>
 
 <template>
+  <Loader :visible="isLoading" />
+
   <div v-if="product">
-    <ProductBanner :data="product"/>
+    <ProductBanner :data="product" />
   </div>
   <div v-if="product">
-    <ProductDescriptions class="text-baseColor" :data="product"/>
+    <ProductDescriptions class="text-baseColor" :data="product" />
   </div>
   <div id="banner">
-    <SliderV2 :items="sliderv2"/>
+    <SliderV2 :items="sliderv2" />
   </div>
   <div>
-    <HowToFind/>
+    <HowToFind />
   </div>
 </template>
