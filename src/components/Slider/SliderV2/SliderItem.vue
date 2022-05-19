eb62915176821e6136177bb3f64054f926f85011
Shafly Hamzah <shafly2000@gmail.com>
2022-05-19 14:16:07 +0700

    increase sliderv2 margin

--- src/components/Slider/SliderV2/SliderItem.vue
+++ src/components/Slider/SliderV2/SliderItem.vue
@@ -1,30 +1,32 @@
 <script setup lang="ts">
-import { defineProps, computed, toRefs } from 'vue'
-import { SliderV2 } from '../../../typings/SliderV2'
+import { defineProps, computed, toRefs } from "vue";
+import { SliderV2 } from "../../../typings/SliderV2";
 
 interface Prop {
-    data: SliderV2;
-    currentSlide: number;
-    index: number;
-    direction: string;
+  data: SliderV2;
+  currentSlide: number;
+  index: number;
+  direction: string;
 }
 
-const props = defineProps<Prop>()
+const props = defineProps<Prop>();
 
-const { direction, data } = toRefs(props)
+const { direction, data } = toRefs(props);
 
 const transitionEffect = computed(() => {
-    return direction.value === 'right' ? 'slide-out' : 'slide-in'
-})
+  return direction.value === "right" ? "slide-out" : "slide-in";
+});
 </script>
 
-
 <template>
-    <transition :name="transitionEffect">
-        <div class="absolute mx-6 sm:mx-12 top-1/2 -translate-y-1/2" v-show="currentSlide === index">
-            <img :src="data.source" :alt="`slide-${index}`"  />
-        </div>
-    </transition>
+  <transition :name="transitionEffect">
+    <div
+      class="absolute mx-6 sm:mx-16 top-1/2 -translate-y-1/2"
+      v-show="currentSlide === index"
+    >
+      <img :src="data.source" :alt="`slide-${index}`" />
+    </div>
+  </transition>
 </template>
 
 <style scoped>
@@ -32,22 +34,22 @@ const transitionEffect = computed(() => {
 .slide-in-leave-active,
 .slide-out-enter-active,
 .slide-out-leave-active {
-    transition: all 1s ease-in-out;
+  transition: all 1s ease-in-out;
 }
 .slide-in-enter-from {
-    margin: 15px;
-    transform: translate(-100%,-55%);
+  margin: 15px;
+  transform: translate(-100%, -55%);
 }
 .slide-in-leave-to {
-    margin: 15px;
-    transform: translate(100%,-55%);
+  margin: 15px;
+  transform: translate(100%, -55%);
 }
 .slide-out-enter-from {
-    margin: 15px;
-    transform: translate(100%,-55%);
+  margin: 15px;
+  transform: translate(100%, -55%);
 }
 .slide-out-leave-to {
-    margin: 15px;
-    transform: translate(-100%,-55%);
+  margin: 15px;
+  transform: translate(-100%, -55%);
 }
 </style>
--- src/components/Slider/SliderV2/SliderV2.vue
+++ src/components/Slider/SliderV2/SliderV2.vue
@@ -1,88 +1,88 @@
 <script setup lang="ts">
-import { ref, defineProps, toRefs, onBeforeUnmount, onMounted } from 'vue'
+import { ref, defineProps, toRefs, onBeforeUnmount, onMounted } from "vue";
 // @ts-ignore
-import SliderItem from './SliderItem.vue';
+import SliderItem from "./SliderItem.vue";
 // @ts-ignore
-import SliderButton from './SliderButton.vue';
-import { SliderV2 } from '../../../typings/SliderV2'
+import SliderButton from "./SliderButton.vue";
+import { SliderV2 } from "../../../typings/SliderV2";
 
 interface Prop {
-    items: SliderV2[]
+  items: SliderV2[];
 }
 
-const props = defineProps<Prop>()
-const { items } = toRefs(props)
-const currentSlide = ref(0)
-const slideInterval = ref<NodeJS.Timer>()
-const direction = ref("right")
+const props = defineProps<Prop>();
+const { items } = toRefs(props);
+const currentSlide = ref(0);
+const slideInterval = ref<NodeJS.Timer>();
+const direction = ref("right");
 
-const banner = ref<any>()
-const bannerWidth = ref(0)
+const banner = ref<any>();
+const bannerHeight = ref(0);
 
 function setCurrentSlide(index: number) {
-    currentSlide.value = index
+  currentSlide.value = index;
 }
 
 function prev() {
-    const index = currentSlide.value > 0 ? currentSlide.value - 1 : items.value.length - 1
-    setCurrentSlide(index)
-    direction.value = "left"
-    startSlideTimer()
+  const index =
+    currentSlide.value > 0 ? currentSlide.value - 1 : items.value.length - 1;
+  setCurrentSlide(index);
+  direction.value = "left";
+  startSlideTimer();
 }
 
 function _next() {
-    const index = currentSlide.value < items.value.length - 1 ? currentSlide.value + 1 : 0
-    setCurrentSlide(index)
-    direction.value = "right"
+  const index =
+    currentSlide.value < items.value.length - 1 ? currentSlide.value + 1 : 0;
+  setCurrentSlide(index);
+  direction.value = "right";
 }
 
 function next() {
-    _next()
-    startSlideTimer()
+  _next();
+  startSlideTimer();
 }
 
 function startSlideTimer() {
-    stopSliderTimer()
-    slideInterval.value = setInterval(() => {
-        next()
-    }, 5000)
+  stopSliderTimer();
+  slideInterval.value = setInterval(() => {
+    next();
+  }, 6000);
 }
 
 function stopSliderTimer() {
-    clearInterval(slideInterval.value!)
+  clearInterval(slideInterval.value!);
 }
 
 onMounted(function () {
-    // startSlideTimer()
-    setTimeout(() => {
-        bannerWidth.value = banner.value.offsetWidth / 1.96;
-        console.log(bannerWidth.value)
-    }, 500);
-})
+  // startSlideTimer()
+  setTimeout(() => {
+    bannerHeight.value = banner.value.offsetWidth / 1.8;
+    console.log(bannerHeight.value);
+  }, 500);
+});
 
 onBeforeUnmount(function () {
-    stopSliderTimer()
-})
-
+  stopSliderTimer();
+});
 </script>
 
 <template>
-    <div class="flex justify-center">
-        <div
-            ref="banner"
-            class="relative w-full overflow-hidden bg-neutral-100"
-            :style="`height: ${bannerWidth}px;`"
-        >
-            <SliderItem
-                v-for="(item, index) in items"
-                :data="item"
-                :key="`slide-${index}`"
-                :current-slide="currentSlide"
-                :index="index"
-                :direction="direction"
-            ></SliderItem>
-            <SliderButton @prev="prev" @next="next"></SliderButton>
-        </div>
+  <div class="flex justify-center">
+    <div
+      ref="banner"
+      class="relative w-full overflow-hidden bg-neutral-100"
+      :style="`height: ${bannerHeight}px;`"
+    >
+      <SliderItem
+        v-for="(item, index) in items"
+        :data="item"
+        :key="`slide-${index}`"
+        :current-slide="currentSlide"
+        :index="index"
+        :direction="direction"
+      ></SliderItem>
+      <SliderButton @prev="prev" @next="next"></SliderButton>
     </div>
+  </div>
 </template>
-
