fdca1d9d336dfe56ce158225e2fc657e201bf93e
Shafly Hamzah <shafly2000@gmail.com>
2022-05-25 00:48:35 +0700

    add responsitivity

--- src/components/SearchResultProduct.vue
+++ src/components/SearchResultProduct.vue
@@ -4,25 +4,24 @@
 <template>
     <div class="w-full mt-12">
         <div class="flex flex-row w-full">
-            <div class="h-52 w-60 flex-none bg-hydrate">
+            <div class="h-52 w-[25%] flex-none bg-hydrate">
                 <img class="h-full w-full p-4 object-contain" src="/img/hydrate-series.png" />
                 <div
                     class="text-center tracking-[0.5em] font-inter text-hydrate border-[1px] border-hydrate py-2 pl-2 mt-6">
                     HYDRATE </div>
             </div>
             <div class="flex-1">
-                <div class="h-52 w-full flex flex-col bg-white-smoke font-questrial p-14">
+                <div class="h-52 w-full flex flex-col justify-center bg-white-smoke font-questrial p-8 lg:p-14">
                     <h2 class="text-4xl text-hydrate">Caffeine Hydrating</h2>
-                    <p class="text-baseColor mt-4">Caffeine is a powerful antioxidant and anti-aging.<br>
+                    <p class="text-baseColor mt-4 max-w-sm">Caffeine is a powerful antioxidant and anti-aging.
                         While combined with green tea and polypheno.</p>
                 </div>
                 <div class="flex flex-row align-middle gap-6 mt-6 ml-6 text-hydrate font-questrial ">
-                    <img class="w-10 h-10" src="/img/icon1.png">
-                    <img class="w-10 h-10" src="/img/icon2.png">
+                    <img class="w-6 md:w-10 h-6 md:h-10" src="/img/icon1.png">
+                    <img class="w-6 md:w-10 h-6 md:h-10" src="/img/icon2.png">
                     <div class="text-center my-auto">Powerful hydration and anti-aging</div>
                 </div>
             </div>
-
         </div>
     </div>
 
