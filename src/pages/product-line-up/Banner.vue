908f13f2efd5ae726119ca7182cb4fb2cc60b5d3
dalfiannur <dalfiannur@gmail.com>
2022-06-03 11:14:58 +0700

    Rev Banner Product Lineup

--- src/pages/product-line-up/Banner.vue
+++ src/pages/product-line-up/Banner.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="bg-hydrate lg:h-[50vh] 2xl:h-[60vh]">
+  <div class="bg-hydrate lg:h-[50vh] 2xl:h-[60vh] overflow-hidden">
     <div
       class="relative py-5 flex items-center justify-center h-full w-full flex-grow"
     >
