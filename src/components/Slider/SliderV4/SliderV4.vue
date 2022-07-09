0e5ad4cbfb8dde632a150a1b1a87bb1314c23fdc
yudaono <yudaono@gmail.com>
2022-07-09 20:02:47 +0700

    update

--- src/components/Slider/SliderV4/SliderV4.vue
+++ src/components/Slider/SliderV4/SliderV4.vue
@@ -26,13 +26,6 @@ import ProductButton from './ProductButton.vue';
                 <p class="text-base text-white font-questrial mt-12 mb-4">Caffeine is a powerful antioxidant and
                     anti-aging. While combined with green tea and polyphenol, caffeince can also.</p>
             </div>
-<<<<<<< HEAD
-=======
-            <div class="relative w-full md:w-6/12 lg:w-7/12">
-                <img class="md:absolute right-0 top-1/4 md:-translate-y-1/2 max-h-[500px] max-w-full"
-                    src="/img/BannerLineUp2.png" alt="product-line-up" />
-            </div>
->>>>>>> 8425cf63d80045de4b05cc3f93cb71f5374f0c04
         </div>
     </div>
 </template>
